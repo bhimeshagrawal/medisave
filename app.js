d8d778db5bc1d6d78c641f9e90525a02c8499fe1
bhimeshagrawal <bhimeshagrawalggc@gmail.com>
2021-09-13 10:39:05 +0530

    feat: redesigned app page

---
+++ app.js
@@ -0,0 +1,14 @@
+
+$(".upload-menu-dropdown-btn").click(function(){
+    $(".upload-window").toggleClass("showme");
+    $(".upload-menu-dropdown-btn").toggleClass("rotate")
+})
+$(".menu-btn").click(function(){
+    $(".left-side-navbar").toggleClass("showmenu");
+    $(".cancel-btn").toggleClass("showme");
+})
+$(".cancel-btn").click(function(){
+    $(".left-side-navbar").toggleClass("showmenu");
+    $(".cancel-btn").toggleClass("showme");
+    
+})
